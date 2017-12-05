e04180bcf38d0b3329de0d597b153b746e54419c
GOPS-XYZ <vmumbarekar@gmail.com>
2017-12-05 13:14:23 +0530

    Add files via upload

--- speechR.py
+++ speechR.py
@@ -1,10 +1,8 @@
 import speech_recognition as sr
 
-for index, name in enumerate(sr.Microphone.list_microphone_names()):
-    print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
-import pyaudio
-p = pyaudio.__version__
-print(p)
+##for index, name in enumerate(sr.Microphone.list_microphone_names()):
+##    print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
+
 # Record Audio
 r = sr.Recognizer()
 while True:
