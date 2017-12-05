64bf2e22e957f7a30e5088a630fca325395f6688
GOPS-XYZ <vmumbarekar@gmail.com>
2017-12-05 13:01:55 +0530

    Add files via upload

---
+++ sampleTTS.py
@@ -0,0 +1,8 @@
+from gtts import gTTS
+#import os
+tts = gTTS(text='Q2a3s4d5f yvyctxez rcubknpmp pkp kohg 6d4s 2a2a4xyvinoz nonnw9nbw8vw7b soosn9d j9j ek9 ej8h28h27v2ubuw beonosnpmdondn odnon8 eb8eb8he8 ejomdonond bwib7wb 7ev7ben osnnsibs8b8 eb8hs8j9s n9sn 9sj9s n9js9 jsjs9js 9n sonsonsoms pmspne 9nene 9b9e  eomp emepm spmepme one9 ne9n e9en9s 9een9 nne9n e9ne0 sns 0nsono snosno wno wnwownpw nonsjs9h9wb8bwbw8bw b8w b8wb8 hw8hwh', lang='es')
+tts.save("wtf.mp3")
+#os.system("mpg321 good.mp3")
+
+
+
---
+++ speechR.py
@@ -0,0 +1,26 @@
+import speech_recognition as sr
+
+for index, name in enumerate(sr.Microphone.list_microphone_names()):
+    print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
+import pyaudio
+p = pyaudio.__version__
+print(p)
+# Record Audio
+r = sr.Recognizer()
+while True:
+    with sr.Microphone(device_index=0) as source:
+        r.adjust_for_ambient_noise(source)
+        print("Say something!")
+        audio = r.listen(source,timeout=8,phrase_time_limit=8)
+        print("done")
+
+    # Speech recognition using Google Speech Recognition
+    try:
+        # for testing purposes, we're just using the default API key
+        # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+        # instead of `r.recognize_google(audio)`
+        print("You said: " + r.recognize_google(audio))
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
